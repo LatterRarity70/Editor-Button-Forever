50275e99a1029a5a01c182a291a9c99c797b754b
user666 <user95401@gmail.com>
2025-01-08 20:19:28 +0400

    Добавьте файлы проекта.

---
+++ about.md
@@ -0,0 +1,3 @@
+# Editor Button Forever
+
+Basically, for fan-made mods that don't allow you to go to the creator layer. This mod adds a popup to choose whether to open the CreatorLayer or to call the original callback.
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Geode/Geode.hpp>
+#include "_updater.hpp"
+
+using namespace geode::prelude;
+
+#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
+class $modify(GJButtonsExt, CCMenuItemSpriteExtra) {
+	$override void activate() {
+
+		if (this->getID() == "editor-button" and CCScene::get()->getChildByIDRecursive("MenuLayer")) {
+			return (void)createQuickPopup(
+				"Select Action", 
+				"Open <cy>CreatorLayer</c> or call <co>current callback</c>?",
+				"CreatorLayer", "Callback", [this](CCNode*, bool btn2) {
+					if (!this) return;
+					if (btn2) return ((m_pListener ? m_pListener : this)->*m_pfnSelector)(this);
+					switchToScene(CreatorLayer::create());
+				}
+			);
+		}
+
+		return CCMenuItemSpriteExtra::activate();
+	}
+};
